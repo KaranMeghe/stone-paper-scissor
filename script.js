7ed000ba41d55617c544b90b11c1b77be18e1d6d
Karan Meghe <karanmeghe87@gmail.com>
2022-11-17 20:03:11 +0530

    functions updates for game

--- script.js
+++ script.js
@@ -1,66 +1,107 @@
-// Values for game
-const gameValues = ["stone", "paper", "scissor"];
+// Select Dom Elements
+const choices = document.querySelectorAll('.choices');
+const score = document.getElementById('score');
+const restart = document.getElementById('restart');
+const result = document.getElementById('result');
+const modal = document.querySelector('.modal');
 
-// get computer choice
-function getComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3);
-  let result;
-  if (randomNumber === 0) {
-    result = "Stone";
-  } else if (randomNumber === 1) {
-    result = "Paper";
+// Scoreboard
+const scoreBoard = {
+  player: 0,
+  computer: 0
+};
+
+// Play Game function()
+const game = (event) => {
+  restart.style.display = 'inline-block';
+  const playerChoice = event.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice,computerChoice);
+  showWinner(winner,computerChoice);
+  console.log(playerChoice,computerChoice,winner);
+}
+
+// Get Computer Choice()
+const getComputerChoice = () => {
+  const randomNumber = Math.random();
+  if(randomNumber < 0.34){
+    return 'rock';
+  } else if(randomNumber <= 0.67){
+    return 'paper';
   } else {
-    result = "Scissor";
-  }
-  return result.toLowerCase();
-}
-
-// get player choice
-function getPlayerChoice() {
-  let playerTurn = prompt("Enter your choice").toLowerCase();
-  return playerTurn;
-}
-
-let computerWin = 1;
-let playerWin = 1;
-
-// playing functionality
-function playRound(playerSelection, computerSelection) {
-  let result;
-  if (!gameValues.includes(playerSelection)) {
-    return (result = `${playerSelection} is invalid input`);
-  }
-
-  if (
-    (playerSelection === "stone" && computerSelection === "scissor") ||
-    (playerSelection === "paper" && computerSelection === "stone") ||
-    (playerSelection === "scissor" && computerSelection === "paper")
-  ) {
-    return (result = `Player choice : ${playerSelection}, Computer choice : ${computerSelection}: ${playerSelection} wins, player ${playerWin++} times win`);
-  } else if (
-    (computerSelection === "stone" && playerSelection === "scissor") ||
-    (computerSelection === "paper" && playerSelection === "stone") ||
-    (computerSelection === "scissor" && playerSelection === "paper")
-  ) {
-    return (result = `Player choice : ${playerSelection}, Computer choice : ${computerSelection}: ${computerSelection} wins computer ${computerWin++} times win`);
-  } else if (playerSelection === computerSelection) {
-    return (result = `Same choices, try again`);
+    return 'scissors';
   }
 }
 
-function game(round) {
-  for (let i = 1; i < 6; i++) {
-    console.log(`Round: ${round++} `);
-    const playerChoice = getPlayerChoice();
-    const computerChoice = getComputerChoice();
-    console.log(playRound(playerChoice, computerChoice));
-  }
-  if (playerWin > computerWin) {
-    console.log(`Player wins the game`);
-  } else if (computerWin > playerWin) {
-    console.log(`Computer wins the game`);
-  } else if (playerWin === computerWin) {
-    console.log("its Draw");
+// Get winner()
+const getWinner = (p,c) => {
+if(p === c) return 'draw';
+// rock & paper
+if(p === 'rock' && c === 'paper') return 'computer';
+if(p === 'paper' && c === 'rock') return 'player'; 
+
+// paper & scissors
+if(p === 'paper' && c === 'scissors') return 'computer';
+if(p === 'scissors' && c === 'paper') return 'player';
+
+// rock & scissors
+if(p === 'scissors' && c === 'rock') return 'computer'; 
+if(p === 'rock' && c === 'scissors') return 'player'; 
+
+}
+
+// Display Winner
+const showWinner = (winner, computerChoice) => {
+// player wins
+if(winner === 'player'){
+  scoreBoard.player++;
+  // display modal result
+  result.innerHTML = `<h1 class = 'text-win'>You Win</h1> 
+  <i class="fas fa-hand-${computerChoice} fa-10x"></i> 
+  <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
+
+} else if(winner === 'computer') {
+  // com wins
+  scoreBoard.computer++;
+  // display modal result
+  result.innerHTML = `<h1 class = 'text-lose'>You Lose</h1> 
+  <i class="fas fa-hand-${computerChoice} fa-10x"></i> 
+  <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
+
+} else {
+  result.innerHTML = `<h1>Its Draw</h1> 
+  <i class="fas fa-hand-${computerChoice} fa-10x"></i> 
+  <p>Computer Chose <strong>${computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1)}</strong></p>`;
+}
+
+// Show score
+ score.innerHTML = `
+ <p>Player: ${scoreBoard.player}</p>
+ <p>Computer: ${scoreBoard.computer}</p>
+ `;
+
+ modal.style.display = 'block';
+}
+
+// clear modal
+function clearModal(event){
+  if(event.target === modal){
+    modal.style.display = 'none';
   }
 }
-game(1);
+
+// Restart function
+function restartGame(){
+scoreBoard.player = 0;
+scoreBoard.computer = 0;
+score.innerHTML = `
+<p>Player: 0</p>
+<p>Computer: 0</p>
+`;
+restart.style.display = 'none';
+}
+
+// Event Listner
+choices.forEach(choice => choice.addEventListener('click', game));
+window.addEventListener('click', clearModal)
+restart.addEventListener('click', restartGame);
